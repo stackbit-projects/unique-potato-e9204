2f3d949a63d44d77ba98e3c6d102e6b1a0cbf393
Stackbit Code Editor <projects@stackbit.com>
2021-12-06 22:50:58 +0000

    register-components.js: changed

--- src/components/register-components.js
+++ src/components/register-components.js
@@ -5,7 +5,7 @@ import { componentsMap } from '@stackbit/components/dist/components-map';
 registerComponents({
     // Register all Stackbit components
     ...componentsMap
-
+AlertSection: dynamic(() => import('./AlertSection'))
     // Override any static or dynamic component,
     // or register your own dynamic component.
 });
